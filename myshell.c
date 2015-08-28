675f0220d8aaf260c9546699fd6261d5eb54239f
Gal Schlezinger <galstar@spitfire.co.il>
2015-08-28 18:17:27 +0300

    ls works

--- myshell.c
+++ myshell.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 #define NUM_OF_ARGS 3
@@ -14,12 +15,19 @@
 
 typedef int CommandType;
 typedef int bool;
+typedef struct Process {
+  int pid;
+  int i;
+  struct Process* next;
+} Process;
 
-char* getCommandName(char* strings);
 char* trimString(char* str, size_t size, int* length);
-char** getCommandArgs();
-void readCommand(char** command, char*** args);
+char** getCommandArgs(char* trimmedCmd);
+void readCommand(char*** args);
 void handleCommand(char* command, char** args, bool* shouldContinue);
+
+int processes = 0;
+Process* firstProcess = NULL;
 
 int main(int cargs, char* argv[]) {
   if (cargs < 2) {
@@ -37,22 +45,72 @@ int main(int cargs, char* argv[]) {
 
   do {
     printf("my_shell_%s>> ", shellName);
-    readCommand(&command, &args);
-    handleCommand(command, args, &shouldContinue);
+    readCommand(&args);
+    if (args == NULL) {
+      command = "";
+    } else {
+      command = args[0];
+    }
+    handleCommand(
+      command,
+      args,
+      &shouldContinue
+    );
+    Process* proc = firstProcess;
+    printf("processes: ");
+    i = 0;
+    while (proc != NULL) {
+      printf("%i ", (int)proc->pid);
+      proc = proc->next;
+      i++;
+    }
+    printf("\n%i\n", i);
   } while (shouldContinue);
 
   return 0;
 };
 
 void handleCommand(char* command, char** args, bool* shouldContinue) {
-  if (0 == strcmp(command, "quit")) {
+  Process* oldProcess;
+  Process* proc;
+  int status;
+  pid_t pid;
+  int executed;
+
+  if (command == NULL) {
+    return;
+  } else if (0 == strcmp(command, "quit")) {
     *shouldContinue = FALSE;
   } else if (0 < strlen(command)) {
     printf("launching command...\n");
+
+    pid = fork();
+    printf("pid = %i\n", pid);
+    if (pid == 0) { // child process
+      executed = execvp(args[0], args);
+      if (executed < 0) {
+        printf("error on execute: %i\n", executed);
+        exit(1);
+      }
+    } else if (pid > 0) { // parent
+      oldProcess = firstProcess;
+      proc = malloc(sizeof(Process));
+      proc->pid = pid;
+      proc->next = oldProcess;
+      proc->i = ++processes;
+      firstProcess = proc;
+      printf("%i\n", processes);
+      while (wait(&status) != pid);
+    } else { // error
+      printf("errors");
+      exit(1);
+    }
+
+    printf("status: %i\n", status);
   }
 }
 
-void readCommand(char** command, char*** args) {
+void readCommand(char*** args) {
   char* line;
   size_t size;
   int trimmedSize;
@@ -60,25 +118,27 @@ void readCommand(char** command, char*** args) {
 
   getline(&line, &size, stdin);
   trimmedCmd = trimString(line, size, &trimmedSize);
-  command[0] = getCommandName(trimmedCmd);
-  args[0] = getCommandArgs();
+  if (strlen(trimmedCmd) > 0) {
+    args[0] = getCommandArgs(trimmedCmd);
+  } else {
+    args[0] = NULL;
+  }
 
   free(line);
   free(trimmedCmd);
 };
 
-char** getCommandArgs() {
+char** getCommandArgs(char* trimmedCmd) {
   int i = 0;
-  char** args = malloc(NUM_OF_ARGS);
+  char** args = malloc(NUM_OF_ARGS + 2);
   char* arg;
-  while ((arg = strtok(NULL, " ")) != NULL) {
+  arg = strtok(trimmedCmd, " ");
+  while (arg != NULL) {
     args[i++] = arg;
+    arg = strtok(NULL, " ");
   }
+  args[i] = NULL;
   return args;
-}
-
-char* getCommandName(char* strings) {
-  return strtok(strings, " ");
 }
 
 char* trimString(char* str, size_t size, int* length) {
